d18923b8222d65a34d5bd0d3de5c8f610b441bc5
Can Özer <45624149+Mucante@users.noreply.github.com>
2018-12-22 23:22:12 +0300

    Add files via upload

--- ThreadServer.java
+++ ThreadServer.java
@@ -1,6 +1,7 @@
 package TriviaQuiz_1;
 
 import java.net.*;
+
 import java.io.*;
 
 class ThreadServer implements Runnable {
@@ -8,68 +9,81 @@ class ThreadServer implements Runnable {
 	private static PrintWriter serverPrintOut;
 	private static boolean closed = false;
 	
+	private static TriviaDB app;
+	static String sendnext;
+	static String quest,aa,bb,cc,dd,correctcorrect;
+	static int randomquestionID;
+
+	  public static void getrandomvalues(int randomquestionlol) {
+  		quest = app.getQuestion(randomquestionlol);
+  		aa = app.getAnswerA(randomquestionlol);
+  		bb = app.getAnswerB(randomquestionlol);
+  		cc = app.getAnswerC(randomquestionlol);
+  		dd = app.getAnswerD(randomquestionlol);
+  		correctcorrect = app.getcorrectAnswer(randomquestionlol);
+      }   
+		public static void settoclient(){
+			randomquestionID=app.getRandomNumber();
+			getrandomvalues(randomquestionID);
+			serverPrintOut.println(quest);
+			serverPrintOut.println(aa);
+			serverPrintOut.println(bb);
+			serverPrintOut.println(cc);
+			serverPrintOut.println(dd);
+			serverPrintOut.println(correctcorrect);
+		}
+
 	public static void main(String args[]) throws Exception {
-		TriviaDB app = new TriviaDB();
+		app = new TriviaDB();
 		ServerSocket ss = new ServerSocket(3333);
 		Socket s = ss.accept();
 		
 		inFromClient = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		serverPrintOut = new PrintWriter(s.getOutputStream(), true);
-		
+        
 		String clientSentence = "";
-		
 		new Thread(new ThreadServer()).start();
-		while (!clientSentence.equals("stop")) 
-		{		
-			int randomquestionID=app.getRandomNumber();		
-			String qstion=app.getQuestion(randomquestionID);
-			String answera=app.getAnswerA(randomquestionID);
-			String answerb=app.getAnswerB(randomquestionID);
-			String answerc=app.getAnswerC(randomquestionID);
-			String answerd=app.getAnswerD(randomquestionID);
-			serverPrintOut.println(qstion);
-			serverPrintOut.println("A) "+answera);
-			serverPrintOut.println("B) "+answerb);
-			serverPrintOut.println("C) "+answerc);
-			serverPrintOut.println("D) "+answerd);
-			
-			clientSentence = inFromClient.readLine();
-			String correctanswer=app.getcorrectAnswer(randomquestionID);
+		//here we go :)
+        settoclient();
 
-			if(clientSentence.equals(correctanswer)){
-				serverPrintOut.println("Congrats! You got the right answer which is: "+clientSentence);
-			}
-			else{
-				serverPrintOut.println("Nope... The correct answer was: "+correctanswer);
-			}
-			serverPrintOut.flush();
-		}
 		
+		while (!clientSentence.equals("stop")) 
+		{	
+			
+				settoclient();
+			           
+			//clientSentence = inFromClient.readLine();
+			//serverPrintOut.flush();
+		}
 		System.out.println("closing");
 		inFromClient.close();
 		s.close();
 		ss.close();
 		closed = true;
 		System.exit(0);
-	}
+	}//end of main
 	
 	public void run() {
-		/*
+
+			/*
 		 * Keep on reading from the socket till we receive "Bye" from the
 		 * server. Once we received that then we want to break.
 		 */
-		String responseLine;
-		try {
-			System.out.println("Waiting");
-			while ((responseLine = inFromClient.readLine()) != null) {
-				System.out.println(responseLine);
-				if (responseLine.indexOf("*** Bye") != -1)
-					break;
-				if (closed)
-					break;
-			}
-		} catch (IOException e) {
-			System.err.println("IOException:  " + e);
-		}
-	}
-}
+		
+		//String responseLine;
+		//try {
+		//	System.out.println("Waiting");
+		//	while ((responseLine = inFromClient.readLine()) != null) {
+		//		System.out.println(responseLine);
+		//		if (responseLine.indexOf("*** Bye") != -1)
+		//   		break;
+		//		if (closed)
+		//			break;
+		//	}
+	//	} catch (IOException e) {
+	//		System.err.println("IOException:  " + e);
+	//	}
+		
+	
+	}//end of run
+}//end of class
