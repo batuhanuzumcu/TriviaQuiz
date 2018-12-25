b6e58a6818215946d395d44587c420e30b35a08a
Can Özer <45624149+Mucante@users.noreply.github.com>
2018-12-25 17:53:51 +0300

    Add files via upload

--- ThreadClient.java
+++ ThreadClient.java
@@ -16,11 +16,11 @@ public class ThreadClient implements Runnable {
 	private static BufferedReader inFromServer = null; // The input stream
 	private static BufferedReader inFromUser = null;
 	private static DataOutputStream outToServer = null;
-
-	private static boolean closed = false;
+    
+	private volatile static int numberofclicks;
 	private static String questioon,ansa,ansb,ansc,ansd,correctans;
 	
-	public static void getfromserver() throws IOException {
+	public static synchronized void getfromserver() throws IOException {
 		questioon=inFromServer.readLine();
 		System.out.println(questioon);
 		ansa=inFromServer.readLine();
@@ -35,9 +35,22 @@ public class ThreadClient implements Runnable {
 		System.out.println(correctans);
 	}
 	
-	public static void main(String[] args) throws ClassNotFoundException {
+	//public static void waiting(){
+	//	if(numberofclicks!=4){
+	//		
+	//	}
+	//		if(numberofclicks==4){
+	//			//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+	//			os.println("ready");
+	//			numberofclicks=0;		
+	//	}
+    //
+	//}
+	
+	public static void main(String[] args) {
+		numberofclicks=0;
 		int portNumber = 3333;
-		String host = "192.168.1.11";
+		String host = "localhost";
 
 		// Open a socket on a given host and port. Open input and output streams.
 		try {
@@ -53,168 +66,302 @@ public class ThreadClient implements Runnable {
 			System.err.println("Couldn't get I/O for the connection to the host " + host);
 		}
 
-		/*
-		 * If everything has been initialized then we want to write some data to
-		 * the socket we have opened a connection to on the port portNumber.
-		 */
+
 		//buraya kadar np sonrasinda thread yaratimi ve managementi var.
 		
-		
-		
-		
-		
+				
 		if (clientSocket != null && os != null && inFromServer != null) {
-			try {		
-				/* Create a thread to read from the server. */
-				new Thread(new ThreadClient()).start();
-				//here we go boii :)
-				
-			    getfromserver();
-				GUItest alpha = new GUItest();
-				alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-				alpha.A.addActionListener(new ActionListener() {
-
-					public void actionPerformed(ActionEvent e) {
-						if (alpha.correctanswer.equals("A")) {
-
-							JOptionPane.showMessageDialog(alpha.frame, "Correct!");
-							
-							try {
-								getfromserver();
-								alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-						else {
-							JOptionPane.showMessageDialog(alpha.frame, "Wrong!");
-							
-					    try {
-							getfromserver();
-							alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-
-						}
-					}
-				});
-				
-				alpha.B.addActionListener(new ActionListener() {
-
-					public void actionPerformed(ActionEvent e) {
-						if (alpha.correctanswer.equals("B")) {
-
-							JOptionPane.showMessageDialog(alpha.frame, "Correct!");
-								
-							try {
-								getfromserver();
-								alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-						else {
-							JOptionPane.showMessageDialog(alpha.frame, "Wrong!");
-							
-							try {
-								getfromserver();
-								alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-
-						}
-					}
-				});
-				
-				alpha.C.addActionListener(new ActionListener() {
-
-					public void actionPerformed(ActionEvent e) {
-						if (alpha.correctanswer.equals("C")) {
-
-							JOptionPane.showMessageDialog(alpha.frame, "Correct!");
-							
-							try {
-								getfromserver();
-								alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-
-						}
-						else {
-							JOptionPane.showMessageDialog(alpha.frame, "Wrong!");
-							
-							try {
-								getfromserver();
-								alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-
-						}
-					}
-				});
-				
-				alpha.D.addActionListener(new ActionListener() {
-
-					public void actionPerformed(ActionEvent e) {
-						if (alpha.correctanswer.equals("D")) {
-
-							JOptionPane.showMessageDialog(alpha.frame, "Correct!");
-							
-							try {
-								getfromserver();
-								alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-
-						}
-						else {
-							JOptionPane.showMessageDialog(alpha.frame, "Wrong!");
-							
-							try {
-								getfromserver();
-								alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-
-						}
-					}
-				});
-						
-				while (!closed) {						
-					//String answer = inFromUser.readLine();
-					//outToServer.writeBytes(answer+'\n');
-					//os.println(answer+'\n');													
-				}
-				// Close the output stream, close the input stream, close the socket
-				os.close();
-				inFromServer.close();
-				outToServer.close();
-				clientSocket.close();
-			} catch (IOException e) {
-				System.err.println("IOException:  " + e);
-			}
+			new Thread(new ThreadClient()).start();				
 		}
-	}
+	}//end of main
+	@Override
+	public void run() {		
+		try {
+			getfromserver();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} 
+		GUItest alpha = new GUItest();
+		alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+		//JOptionPane.showMessageDialog(alpha.frame, "Welcome to the game! Please Wait for others to connect.");
 
-	// Create a thread to read from the server. 
-	public void run() {
-		// Keep on reading from the socket till we receive "Bye" from the
-		// server. Once we received that then we want to break.
-		//bu rundaki son commentleri salla su an run bos kalcak xddd
+		alpha.A.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				if (alpha.correctanswer.equals("A")) {
+					//JOptionPane.showMessageDialog(alpha.frame, "Correct! Wait for other players.");			
+                    numberofclicks++;
+                   System.out.println(numberofclicks);
+					try {
+						if(numberofclicks!=4){
+							synchronized (alpha){
+								try {
+									alpha.wait();
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						}
+						if(numberofclicks==4){
+								//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+								numberofclicks=0;
+								notifyAll();
+								os.println("ready");
+
+										}
+	                    //waiting();
+						getfromserver();
+						alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} 
+				}
+				else {
+					//JOptionPane.showMessageDialog(alpha.frame, "Wrong! Wait for other players.");		
+                    numberofclicks++;
+                    System.out.println(numberofclicks);
+
+			    try {
+			    	if(numberofclicks!=4){
+						synchronized (alpha){
+							try {
+								alpha.wait();
+							} catch (InterruptedException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+					}
+					if(numberofclicks==4){
+							//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+							numberofclicks=0;
+							notifyAll();
+							os.println("ready");
+
+									}
+                    // waiting();
+					getfromserver();
+					alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				}
+			}
+		});
 		
-	}
-}
+		alpha.B.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				if (alpha.correctanswer.equals("B")) {
+					//JOptionPane.showMessageDialog(alpha.frame, "Correct! Wait for other players.");			
+                    numberofclicks++;
+                    System.out.println(numberofclicks);
+
+					try {
+						if(numberofclicks!=4){
+							synchronized (alpha){
+								try {
+									alpha.wait();
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						}
+						if(numberofclicks==4){
+								//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+								numberofclicks=0;
+								notifyAll();
+								os.println("ready");
+
+										}
+	                    //waiting();
+						getfromserver();
+						alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				else {
+					//JOptionPane.showMessageDialog(alpha.frame, "Wrong! Wait for other players.");		
+                    numberofclicks++;
+                    System.out.println(numberofclicks);
+
+					try {
+						if(numberofclicks!=4){
+							synchronized (alpha){
+								try {
+									alpha.wait();
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						}
+						if(numberofclicks==4){
+								//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+								numberofclicks=0;
+								notifyAll();
+								os.println("ready");
+
+										}
+	                    //waiting();
+						getfromserver();
+						alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
+			}
+		});
+		
+		alpha.C.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				if (alpha.correctanswer.equals("C")) {
+					//JOptionPane.showMessageDialog(alpha.frame, "Correct! Wait for other players.");		
+                    numberofclicks++;
+                    System.out.println(numberofclicks);
+
+					try {
+						if(numberofclicks!=4){
+							synchronized (alpha){
+								try {
+									alpha.wait();
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						}
+						if(numberofclicks==4){
+								//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+								numberofclicks=0;
+								notifyAll();
+								os.println("ready");
+
+										}
+	                   // waiting();
+						getfromserver();
+						alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
+				else {
+					//JOptionPane.showMessageDialog(alpha.frame, "Wrong! Wait for other players.");		
+                    numberofclicks++;
+                    System.out.println(numberofclicks);
+
+					try {
+						if(numberofclicks!=4){
+							synchronized (alpha){
+								try {
+									alpha.wait();
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						}
+						if(numberofclicks==4){
+								//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+								numberofclicks=0;
+								notifyAll();
+								os.println("ready");
+
+										}
+	                   // waiting();
+						getfromserver();
+						alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
+			}
+		});
+		
+		alpha.D.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (alpha.correctanswer.equals("D")) {
+					//JOptionPane.showMessageDialog(alpha.frame, "Correct! Wait for other players.");			
+                    numberofclicks++;
+                    System.out.println(numberofclicks);
+
+					try {
+						if(numberofclicks!=4){
+							synchronized (alpha){
+								try {
+									alpha.wait();
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						}
+						if(numberofclicks==4){
+								//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+								numberofclicks=0;
+								notifyAll();
+								os.println("ready");
+
+										}
+	                    //waiting();
+						getfromserver();
+						alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
+				else {
+					//JOptionPane.showMessageDialog(alpha.frame, "Wrong! Wait for other players.");		
+                    numberofclicks++;
+                    System.out.println(numberofclicks);
+
+					try {
+						if(numberofclicks!=4){
+							synchronized (alpha){
+								try {
+									alpha.wait();
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						}
+						if(numberofclicks==4){
+								//JOptionPane.showMessageDialog(alpha.frame, "everyone ready to get next question!");
+								numberofclicks=0;
+								notifyAll();
+								os.println("ready");
+
+										}
+	                    //waiting();
+						getfromserver();
+						alpha.SetGUI(questioon, ansa, ansb, ansc, ansd, correctans);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
+			}
+		});
+		
+	}//end of run
+	
+}//end of class
--- ThreadServer.java
+++ ThreadServer.java
@@ -1,19 +1,20 @@
 package TriviaQuiz_1;
 
 import java.net.*;
-
 import java.io.*;
 
 class ThreadServer implements Runnable {
-	private static BufferedReader inFromClient;
-	private static PrintWriter serverPrintOut;
-	private static boolean closed = false;
+	static BufferedReader inFromClient;
+	static PrintWriter serverPrintOut;
 	
 	private static TriviaDB app;
-	static String sendnext;
 	static String quest,aa,bb,cc,dd,correctcorrect;
-	static int randomquestionID;
+	static int randomquestionID,connectionnumber;
+	static String SendNextQuestion;
+	static int[] IDarray = new int[10];
+	//static Thread[] threads = new Thread[4];
 
+    
 	  public static void getrandomvalues(int randomquestionlol) {
   		quest = app.getQuestion(randomquestionlol);
   		aa = app.getAnswerA(randomquestionlol);
@@ -22,9 +23,11 @@ class ThreadServer implements Runnable {
   		dd = app.getAnswerD(randomquestionlol);
   		correctcorrect = app.getcorrectAnswer(randomquestionlol);
       }   
-		public static void settoclient(){
-			randomquestionID=app.getRandomNumber();
-			getrandomvalues(randomquestionID);
+
+		public static synchronized void settoclient(int fixedquestionID){
+			//randomquestionID=app.getRandomNumber();
+			//getrandomvalues(randomquestionID);
+			getrandomvalues(fixedquestionID);
 			serverPrintOut.println(quest);
 			serverPrintOut.println(aa);
 			serverPrintOut.println(bb);
@@ -35,55 +38,52 @@ class ThreadServer implements Runnable {
 
 	public static void main(String args[]) throws Exception {
 		app = new TriviaDB();
-		ServerSocket ss = new ServerSocket(3333);
-		Socket s = ss.accept();
 		
-		inFromClient = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		serverPrintOut = new PrintWriter(s.getOutputStream(), true);
-        
-		String clientSentence = "";
-		new Thread(new ThreadServer()).start();
-		//here we go :)
-        settoclient();
-
-		
-		while (!clientSentence.equals("stop")) 
-		{	
-			
-				settoclient();
-			           
-			//clientSentence = inFromClient.readLine();
-			//serverPrintOut.flush();
+		for(int count=0;count<IDarray.length;count++){
+			randomquestionID=app.getRandomNumber();
+             IDarray[count]=randomquestionID=app.getRandomNumber();
 		}
-		System.out.println("closing");
-		inFromClient.close();
-		s.close();
-		ss.close();
-		closed = true;
-		System.exit(0);
-	}//end of main
-	
-	public void run() {
+		
+		ServerSocket ss = new ServerSocket(3333);		
+		
+		for (connectionnumber=0;connectionnumber<4;connectionnumber++) {	
+			Socket s = null;
+	        try {
+	            s = ss.accept();
+	            System.out.println("A new client is connected : " + s);
+	             // obtaining input and out streams 
+	            inFromClient = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	    		serverPrintOut = new PrintWriter(s.getOutputStream(), true);
+	            
+	            System.out.println("Assigning new thread for this client"); 
+	             
+	             } catch (IOException e) {
+	            throw new RuntimeException("Error accepting client connection", e);
+	        }    
+	        new Thread(new ThreadServer()).start();
+	        //threads[connectionnumber] = new Thread(new ThreadServer());	  
+	        //threads[connectionnumber].start();
+		}//end of for loop
 
-			/*
-		 * Keep on reading from the socket till we receive "Bye" from the
-		 * server. Once we received that then we want to break.
-		 */
 		
-		//String responseLine;
-		//try {
-		//	System.out.println("Waiting");
-		//	while ((responseLine = inFromClient.readLine()) != null) {
-		//		System.out.println(responseLine);
-		//		if (responseLine.indexOf("*** Bye") != -1)
-		//   		break;
-		//		if (closed)
-		//			break;
-		//	}
-	//	} catch (IOException e) {
-	//		System.err.println("IOException:  " + e);
-	//	}
+	}//end of main
+	@Override
+	public void run() {
+		settoclient(IDarray[0]);
 		
-	
+		for(int i=1;i<10;i++){
+			try {
+				SendNextQuestion=inFromClient.readLine();
+				System.out.println("everyone clicked");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if(SendNextQuestion.equals("ready")){
+				settoclient(IDarray[i]);
+				SendNextQuestion="stop";
+			}
+		}//end of for loop
+		System.out.println("10 questions asked.Ending...");
 	}//end of run
+	
 }//end of class
